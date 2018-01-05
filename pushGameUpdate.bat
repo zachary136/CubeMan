76475401a232758be9d1395b65f9ff50049a2121
zach <zwells147@gmail.com>
2018-01-04 21:02:00 -0600

    normal update

--- pushGameUpdate.bat
+++ pushGameUpdate.bat
@@ -1,9 +1,7 @@
-<<<<<<< HEAD:pushGameUpdate.bat
 git init
+git pull
 git add .
 git commit -m "normal update"
 git push origin master
 pause
-=======
-git pull
->>>>>>> af2ab97c4cce496ba5185636a3f4de697c7fdde6:update.bat
+
